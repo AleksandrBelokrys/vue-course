0abfcc70050e5804712de292f1742d60f20d5943
Oleksandr Bilokrys <oleksandr.bilokrys@raccoongang.com>
2024-05-21 20:48:48 +0300

    initial project setup

---
+++ index.js
@@ -0,0 +1,15 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+function subtractNumbers(a, b) {
+  return a - b;
+}
+
+function multiplyNumbers(a, b) {
+  return a * b;
+}
+
+console.log(addNumbers(5, 3));
+console.log(subtractNumbers(10, 4));
+console.log(multiplyNumbers(2, 6));
