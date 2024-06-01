22366762327d1a57383ee3542121bbdb4172dcac
Oleksandr Bilokrys <oleksandr.bilokrys@raccoongang.com>
2024-06-01 17:07:45 +0300

    added a new component 'CardComponent' that accepts 2 props

--- vue-course/src/App.vue
+++ vue-course/src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div id="app">
+    <CardComponent title="First Card" />
+    <CardComponent text="How are you?" />
+    <CardComponent title="Second Card" text="I'm fine, thank you!" />
   </div>
 </template>
 
 <script>
+import CardComponent from './components/CardComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CardComponent
+  }
+}
 </script>
 
 <style>
---
+++ vue-course/src/components/CardComponent.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "CardComponent",
+  props: {
+    title: String,
+    text: String
+  }
+}
+</script>
+
+<template>
+  <div v-if="title || text" class="card">
+    <div class="card-body">
+      <h4 v-if="title" class="card-title">{{ title }}</h4>
+      <p v-if="text" class="card-text">{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
