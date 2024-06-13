8cfaffde7d43226f35524026855ca77e7a079c60
Oleksandr Bilokrys <oleksandr.bilokrys@raccoongang.com>
2024-06-13 21:47:16 +0300

    added 'AlertComponent' component

--- vue-course/src/App.vue
+++ vue-course/src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <div id="app">
+    <AlertComponent type="primary" text="Primary alert!" />
+    <AlertComponent type="secondary" text="Secondary alert!" />
+    <AlertComponent type="success" text="Success alert!" />
+    <AlertComponent type="danger" text="Danger alert!" />
+    <AlertComponent type="warning" text="Warning alert!" />
+    <AlertComponent type="info" text="Info alert!" />
+    <AlertComponent type="light" text="Light alert!" />
+    <AlertComponent type="dark" text="Dark alert!" />
   </div>
 </template>
 
 <script>
+import AlertComponent from './components/AlertComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AlertComponent
+  }
+};
 </script>
 
 <style>
---
+++ vue-course/src/components/AlertComponent.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: 'AlertComponent',
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true,
+      validator: value => [
+        'primary',
+        'secondary',
+        'success',
+        'danger',
+        'warning',
+        'info',
+        'light',
+        'dark'
+      ].includes(value)
+    }
+  },
+  computed: {
+    alertClasses() {
+      return ['alert', `alert-${this.type}`, 'alert-dismissible', 'fade', 'show'];
+    }
+  }
+};
+</script>
+
+<template>
+  <div :class="alertClasses" role="alert">
+    {{ text }}
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
