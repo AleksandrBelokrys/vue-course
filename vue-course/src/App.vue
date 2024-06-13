180a3afa0c1a346c316f9de53f2e538c67df5984
Oleksandr Bilokrys <oleksandr.bilokrys@raccoongang.com>
2024-06-13 18:17:49 +0300

    added 'DefinitionsList' component

--- vue-course/src/App.vue
+++ vue-course/src/App.vue
@@ -1,9 +1,26 @@
 <template>
   <div id="app">
+    <DefinitionsList :data="definitions" />
   </div>
 </template>
 
 <script>
+import DefinitionsList from './components/DefinitionsList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    DefinitionsList,
+  },
+  data() {
+    return {
+      definitions: [
+        { dt: 'one', dd: 'two', id: 1 },
+        { dt: 'another term', dd: 'another description', id: 2 },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
---
+++ vue-course/src/components/DefinitionsList.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "DefinitionsList",
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <dl>
+    <div v-for="item in data" :key="item.id">
+      <dt>{{ item.dt }}</dt>
+      <dd>{{ item.dd }}</dd>
+    </div>
+  </dl>
+</template>
+<style scoped>
+
+</style>
